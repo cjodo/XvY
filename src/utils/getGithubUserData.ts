bf4420752e893c0f48186d9abe4c969cbbc28175
Curtis <curtisod.123@gmail.com>
2024-05-07 15:45:15 -0600

    Move some user data fetches into components.     a little more stable

--- src/utils/getGithubUserData.ts
+++ src/utils/getGithubUserData.ts
@@ -9,20 +9,20 @@ export const getGithubUserData = async (user: ClerkUserData | null) => {
 }
 
 export const getUserEvents = async (userName: string) => {
-	const res = await fetch(`https://api.github.com/users/${userName}/events`, { cache: 'force-cache' })
+	const res = await fetch(`https://api.github.com/users/${userName}/events`, { next: { revalidate: 3600 } })
 	const events = await res.json()
 
 	return events
 }
 
 export const getRepos = async (userName:string) => {
-	const res = await fetch(`https://api.github.com/users/${userName}/repos`, { cache: "force-cache" })
+	const res = await fetch(`https://api.github.com/users/${userName}/repos`, {next: { revalidate: 3600 }})
 	const commits = await res.json()
 
 	return commits
 }
 export const getCommitsPerRepo = async (repoName: string, userName: string) => {
-	const res = await fetch(`https://api.github.com/repos/${userName}/${repoName}/commits?author=${userName}`, { cache: "force-cache" })
+	const res = await fetch(`https://api.github.com/repos/${userName}/${repoName}/commits?author=${userName}`, { next: { revalidate: 3600 } })
 	const commits = await res.json()
 
 	return commits
