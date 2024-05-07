30d4dd22d44ac9766bf3e41c1aebdd8f56eb43df
Curtis <curtisod.123@gmail.com>
2024-05-07 11:43:30 -0600

    env

--- src/env.js
+++ src/env.js
@@ -11,8 +11,6 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
-    GITHUB_SECRET: z.string(),
-    GITHUB_CLIENT_ID: z.string()
   },
 
   /**
@@ -31,8 +29,6 @@ export const env = createEnv({
   runtimeEnv: {
     POSTGRES_URL: process.env.POSTGRES_URL,
     NODE_ENV: process.env.NODE_ENV,
-    GITHUB_SECRET: process.env.GITHUB_SECRET,
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
