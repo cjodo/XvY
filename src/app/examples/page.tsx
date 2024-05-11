72aa8f6ab10a7749550c84f05bfd5bc8d94e4438
Curtis <curtisod.123@gmail.com>
2024-05-11 17:52:04 -0600

    Add example page, kinda wacked out

---
+++ src/app/examples/page.tsx
@@ -0,0 +1,21 @@
+import { Graph } from "~/_components/Graph/Graph"
+
+export default function ExamplePage() {
+  const topTenContributors = [
+    'c9s',
+    'fabpot',
+    'weierophinney'
+  ]
+
+  return (
+    <>
+      <h2 className="w-full text-center">Because of the GitHub API Rate limit, this page will be buggy :( Working on it</h2>
+      <div>
+      { topTenContributors.map((user) => {
+        return <Graph passedUsername={user}/>
+      })
+      }
+      </div>
+    </>
+  )
+}
