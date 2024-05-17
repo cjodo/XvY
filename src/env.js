4c3befed3ee1ace0fb70812f70c6259b443bf874
Curtis <curtisod.123@gmail.com>
2024-05-16 19:37:40 -0600

    Fix env.js

--- src/env.js
+++ src/env.js
@@ -11,8 +11,6 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
-    PEM: z.string(),
-    GH_CLIENT_ID: z.string()
   },
 
 
@@ -32,8 +30,6 @@ export const env = createEnv({
   runtimeEnv: {
     POSTGRES_URL: process.env.POSTGRES_URL,
     NODE_ENV: process.env.NODE_ENV,
-    PEM: process.env.PEM,
-    GH_CLIENT_ID: process.env.GH_CLIENT_ID
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
