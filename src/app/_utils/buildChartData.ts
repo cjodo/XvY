e45b26d29ab752d80a95a28c6aa8a57a74277712
Curtis <curtisod.123@gmail.com>
2024-05-17 16:27:44 -0600

    W

--- src/app/_utils/buildChartData.ts
+++ src/app/_utils/buildChartData.ts
@@ -3,16 +3,18 @@ import { getCommitsPerRepo } from "./getGithubUserData";
 
 export const buildCommitData = (data: GitRepoData[], withAuth: boolean, token?:string | null) => {
 	let commits:CommitData[] = []; 
-	
-	data.forEach(async (repo: GitRepoData) => {
-		console.log(repo.name)
-		const repoData = await getCommitsPerRepo(repo.name, repo.owner.login, withAuth, token)
-		const amountOfCommits: number = repoData.length
 
-		commits.push({
-			name: repo.name, 
-			amount: amountOfCommits, 
-			repoURL: repo.html_url})
-	})
+	if(data){
+		data.forEach(async (repo: GitRepoData) => {
+			console.log(repo.name)
+			const repoData = await getCommitsPerRepo(repo.name, repo.owner.login, withAuth, token)
+			const amountOfCommits: number = repoData.length
+
+			commits.push({
+				name: repo.name, 
+				amount: amountOfCommits, 
+				repoURL: repo.html_url})
+		})
+	}
 	return commits
 };
