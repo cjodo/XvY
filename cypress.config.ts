cf222499dceed2c412ae0be009372a99217dcb6d
CodingGeeneeus <curtisod.123@gmail.com>
2024-05-27 08:37:33 -0600

    Add auth to testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  env: {
+    gh_username: process.env.GH_USERNAME,
+    gh_password: process.env.GH_PASSWORD,
+  },
   e2e: {
     setupNodeEvents(on, config) { },
+    supportFile: false,
+  },
+  component: {
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack'
+    },
   },
 });
