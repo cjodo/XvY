61f8d2e5139d8a8f1b8c939caf8ef7e668841a39
Curt <curtisod.123@gmail.com>
2024-05-27 08:18:02 -0600

    fix wrong link in user page test

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,6 +1,6 @@
 describe("Navigation", () => {
 	it("Navigate to a repo page", () => {
-		cy.visit("http:localhost:3000/user/REPO");
+		cy.visit("http://localhost:3000/user/REPO");
 
 		cy.get("h2").contains("REPO");
 	});
